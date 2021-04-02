5a93b3e791d4f86d2991c78713383b4367fe7e9d
heatblst <ashishkumar07865apk@gmail.com>
2021-04-02 18:55:21 +0530

    commit on master

--- src/App.js
+++ src/App.js
@@ -1,26 +1,69 @@
-import ModalComponent from "./components/ModalComponent"
-import React from "react";
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
+import React, { useState } from 'react';
+import axios from 'axios';
+import Search from './components/Search';
+import Results from './components/Description';
+import Popup from './components/Popup';
 import "./App.css";
 
-export default function App() {
+function App() {
+  const [state, setState] = useState({
+    s: "",
+    results: [],
+    selected: {}
+  });
+  const apiurl = "https://www.omdbapi.com/?apikey=4d14c2bd";
+
+  const search = (e) => {
+    if (e.key === "Enter") {
+      axios(apiurl + "&s=" + state.s).then(({ data }) => {
+        let results = data.Search;
+
+        setState(prevState => {
+          return { ...prevState, results: results }
+        })
+      });
+    }
+  }
+  
+  const handleInput = (e) => {
+    let s = e.target.value;
+
+    setState(prevState => {
+      return { ...prevState, s: s }
+    });
+  }
+
+  const openPopup = id => {
+    axios(apiurl + "&i=" + id).then(({ data }) => {
+      let result = data;
+
+      console.log(result);
+
+      setState(prevState => {
+        return { ...prevState, selected: result }
+      });
+    });
+  }
+
+  const closePopup = () => {
+    setState(prevState => {
+      return { ...prevState, selected: {} }
+    });
+  }
+
   return (
-    <Router>
-      <div className="App">
-        <nav>
-          <ModalComponent header="Header Name"  body="Body Content" footer="footer Name"/>
-        </nav>
-        <Switch>
-          <Route path="/page#modal" exact>
-          </Route>
-          <Route path="/">
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <div className="App">
+      <header>
+        <h1>Movie Search</h1>
+      </header>
+      <main>
+        <Search handleInput={handleInput} search={search} />
+        <Results results={state.results} openPopup={openPopup} />
+
+        {(typeof state.selected.Title != "undefined") ? <Popup selected={state.selected} closePopup={closePopup} /> : false}
+      </main>
+    </div>
   );
 }
+
+export default App;
